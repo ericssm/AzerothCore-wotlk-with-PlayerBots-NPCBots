e86cbb326a2113583ec7aaa2708d0a3a85d877a8
ericssm <ericsha@sina.com>
2025-10-26 09:31:27 +0800

    update to 20251026

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1492,7 +1492,7 @@ void GameObject::Use(Unit* user)
     // by default spell caster is user
     Unit* spellCaster = user;
     uint32 spellId = 0;
-    bool triggered = false;
+    uint32 triggeredFlags = TRIGGERED_NONE;
 
     if (Player* playerUser = user->ToPlayer())
     {
@@ -1511,6 +1511,10 @@ void GameObject::Use(Unit* user)
 
         m_cooldownTime = GameTime::GetGameTimeMS().count() + cooldown * IN_MILLISECONDS;
     }
+
+    if (user->IsPlayer() && GetGoType() != GAMEOBJECT_TYPE_TRAP) // workaround for GO casting
+        if (!m_goInfo->IsUsableMounted())
+            user->RemoveAurasByType(SPELL_AURA_MOUNTED);
 
     switch (GetGoType())
     {
@@ -1841,14 +1845,14 @@ void GameObject::Use(Unit* user)
                     if (info->summoningRitual.animSpell)
                     {
                         user->CastSpell(user, info->summoningRitual.animSpell, true);
-                        triggered = true;
+                        triggeredFlags = TRIGGERED_FULL_MASK;
                     }
 
                     spellId = info->summoningRitual.spellId;
                     if (spellId == 62330)
                     {
                         spellId = 61993;
-                        triggered = true;
+                        triggeredFlags = TRIGGERED_FULL_MASK;
                     }
                     if (!info->summoningRitual.ritualPersistent)
                         SetLootState(GO_JUST_DEACTIVATED);
@@ -1947,7 +1951,6 @@ void GameObject::Use(Unit* user)
                     }
                 }
 
-                user->RemoveAurasByType(SPELL_AURA_MOUNTED);
                 spellId = info->spellcaster.spellId;
                 break;
             }
@@ -2167,12 +2170,15 @@ void GameObject::Use(Unit* user)
         return;
     }
 
+    if (m_goInfo->IsUsableMounted())
+        triggeredFlags |= TRIGGERED_IGNORE_CASTER_MOUNTED_OR_ON_VEHICLE;
+
     if (Player* player = user->ToPlayer())
         sOutdoorPvPMgr->HandleCustomSpell(player, spellId, this);
 
     if (spellCaster)
     {
-        if ((spellCaster->CastSpell(user, spellInfo, triggered) == SPELL_CAST_OK) && GetGoType() == GAMEOBJECT_TYPE_SPELLCASTER)
+        if ((spellCaster->CastSpell(user, spellInfo, TriggerCastFlags(triggeredFlags)) == SPELL_CAST_OK) && GetGoType() == GAMEOBJECT_TYPE_SPELLCASTER)
             AddUse();
     }
     else
