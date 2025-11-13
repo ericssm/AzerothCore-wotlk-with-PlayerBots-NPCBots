c43191a5cda4661047bc2ad0b6011ec70bb32b41
ericssm <ericsha@sina.com>
2025-11-13 21:36:45 +0800

    update to 20251113

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2056,8 +2056,6 @@ public:
     }
     bool IsMirrorTimerActive(MirrorTimerType type) { return m_MirrorTimer[type] == getMaxTimer(type); }
 
-    void SetMovement(PlayerMovementType pType);
-
     bool CanJoinConstantChannelInZone(ChatChannelsEntry const* channel, AreaTableEntry const* zone);
 
     void JoinedChannel(Channel* c);
