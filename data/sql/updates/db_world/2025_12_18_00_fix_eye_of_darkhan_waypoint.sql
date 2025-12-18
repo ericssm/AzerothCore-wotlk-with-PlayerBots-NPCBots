f6ddc5137e85d06af63bf68bfb810321f0d16554
beyourself <wowshub@gmail.com>
2025-12-18 17:04:06 +0800

    DB update 2025_12_18_00 >> Fix Eye of Dar'Khan waypoint movement error

--- data/sql/updates/db_world/2025_12_18_00_fix_eye_of_darkhan_waypoint.sql
+++ data/sql/updates/db_world/2025_12_18_00_fix_eye_of_darkhan_waypoint.sql
@@ -4,6 +4,8 @@
 --          doesn't have waypoint path id: 0
 --
 -- Solution: Change MovementType from waypoint movement to idle (0)
+--           and set wander_distance to 0 (required when MovementType is 0)
 --           This prevents the error log spam while maintaining the creature's presence
 
-UPDATE `creature` SET `MovementType` = 0 WHERE `id1` = 16320 AND `MovementType` != 0;
+UPDATE `creature` SET `MovementType` = 0, `wander_distance` = 0 WHERE `id1` = 16320;
+
