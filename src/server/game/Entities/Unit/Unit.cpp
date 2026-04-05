cad44bfc4d9d0d894cdc637a1cd7e4e4e72f0658
ericssm <ericsha@sina.com>
2026-04-05 17:17:38 +0800

    update to 20260404 14:44

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1332,7 +1332,8 @@ uint32 Unit::DealDamage(Unit* attacker, Unit* victim, uint32 damage, CleanDamage
         if (!attacker || attacker->IsControlledByPlayer() || attacker->IsCreatedByPlayer())
         {
             uint32 unDamage = health < damage ? health : damage;
-            bool damagedByPlayer = unDamage && attacker && (attacker->IsPlayer() || attacker->m_movedByPlayer != nullptr);
+            bool damagedByPlayer = unDamage && attacker && (attacker->IsPlayer() || attacker->m_movedByPlayer != nullptr
+                || attacker->GetCharmerOrOwnerGUID().IsPlayer());
 #ifdef MOD_NPCERBOTS
             //npcbot: npcbots' damage allways counts towards damage requirement
             damagedByPlayer |= attacker && attacker->IsNPCBotOrPet();
