63097f2862c4f2e5be57ed7c2b77b3e8528da0b7
ericssm <ericsha@sina.com>
2026-04-20 23:00:40 +0800

    update to 20260420 23:00

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1207,9 +1207,6 @@ bool Creature::AIM_Initialize(CreatureAI* ai)
     IsAIEnabled = true;
     i_AI->InitializeAI();
 
-    // Xinef: Initialize vehicle if it is not summoned!
-    if (GetVehicleKit() && m_spawnId)
-        GetVehicleKit()->Reset();
     return true;
 }
 
@@ -2236,6 +2233,10 @@ void Creature::Respawn(bool force)
 
             if (getDeathState() == DeathState::Dead)
             {
+                // TempSummons (no m_spawnId) shouldn't be resurrected here; TempSummon::Update UnSummons them on the next tick once deathState is Dead.
+                if (!m_spawnId && !force)
+                    return;
+
                 if (m_spawnId)
                 {
                     GetMap()->RemoveCreatureRespawnTime(m_spawnId);
