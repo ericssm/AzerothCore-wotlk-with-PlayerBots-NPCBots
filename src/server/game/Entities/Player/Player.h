563a69317be22589830b65d00a3f9decb3339b9f
ericssm <ericsha@sina.com>
2026-07-01 21:38:02 +0800

    update to 20260630 21:38

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2597,6 +2597,7 @@ public:
     Spell* m_spellModTakingSpell;
 
     float GetAverageItemLevel();
+    [[nodiscard]] float GetTotalItemLevel() const;
     float GetAverageItemLevelForDF();
     bool isDebugAreaTriggers;
 
