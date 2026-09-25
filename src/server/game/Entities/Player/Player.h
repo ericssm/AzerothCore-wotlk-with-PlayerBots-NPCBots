f7da6e9a6d1347494ccb1e5cb29b9505346512d8
ericssm <ericsha@sina.com>
2026-09-25 10:04:49 +0800

    update to 20260925 10:05

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2728,6 +2728,8 @@ public:
     bool CanRequestSpellCast(SpellInfo const* spellInfo);
 
 protected:
+    Unit* SelectCharmedAIGroupTarget(float distance) const;
+
     // Gamemaster whisper whitelist
     WhisperListContainer WhisperList;
 
